68e7ab72d1e013c5e71132b589371f4d23d85d2b
jamesacox53 <jamesacox53@gmail.com>
2022-10-06 20:25:48 +0100

    Completed Exercise: Reverse A String

---
+++ Sec 06 - Arrays/Questions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+class Questions
+{
+    public static void Main(string[] args) {
+        string test1 = "Hello World!";
+
+        string reversedTest1 = ReverseString(test1);
+
+        Console.WriteLine(reversedTest1);
+        Console.ReadKey();
+    }
+
+    public static string ReverseString(string input) 
+    {
+        if (String.IsNullOrWhiteSpace(input)) 
+            return input;
+
+        return ReverseStringInput(input);
+    }
+
+    private static string ReverseStringInput(string input)
+    {
+        Stack<char> chars = new Stack<char>();
+
+        foreach (char c in input)
+        {
+            chars.Push(c);
+        }
+
+        char[] reversedChars = chars.ToArray();
+
+        string reversedString = new string(reversedChars);
+
+        return reversedString;
+    }
+
+
+
+
+
+}
