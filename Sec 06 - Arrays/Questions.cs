0bb7ac7ddf4536865ce9313890ffe9f3f934d08d
jamesacox53 <jamesacox53@gmail.com>
2022-10-07 22:11:10 +0100

    Completed Exercise: Implement A Hash Table

--- Sec 06 - Arrays/Questions.cs
+++ Sec 06 - Arrays/Questions.cs
@@ -1,132 +1,136 @@
 ﻿using System.Collections.Generic;
 
-class Questions
+namespace Sec06
 {
-    public static void Main(string[] args) {
-       /*
-        string test1 = "Hello World!";
-
-        string reversedTest1 = ReverseString(test1);
-
-        Console.WriteLine(reversedTest1);
-        */
-
-        List<int> array1 = new List<int> { 0, 3, 4, 31 };
-        List<int> array2 = new List<int> { 4, 6, 30 };
-
-        List<int>? sortedList = MergeSortedArrays(array1, array2);
-
-        foreach(int elem in sortedList)
+    class Questions
+    {
+        public static void Main(string[] args)
         {
-            Console.WriteLine(elem);
+            /*
+             string test1 = "Hello World!";
+
+             string reversedTest1 = ReverseString(test1);
+
+             Console.WriteLine(reversedTest1);
+             */
+
+            List<int> array1 = new List<int> { 0, 3, 4, 31 };
+            List<int> array2 = new List<int> { 4, 6, 30 };
+
+            List<int>? sortedList = MergeSortedArrays(array1, array2);
+
+            foreach (int elem in sortedList)
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadKey();
         }
 
-        Console.ReadKey();
-    }
-
-    public static string ReverseString(string input) 
-    {
-        if (String.IsNullOrWhiteSpace(input)) 
-            return input;
-
-        return ReverseStringInput(input);
-    }
-
-    private static string ReverseStringInput(string input)
-    {
-        Stack<char> chars = new Stack<char>();
-
-        foreach (char c in input)
+        public static string ReverseString(string input)
         {
-            chars.Push(c);
+            if (String.IsNullOrWhiteSpace(input))
+                return input;
+
+            return ReverseStringInput(input);
         }
 
-        char[] reversedChars = chars.ToArray();
-
-        string reversedString = new string(reversedChars);
-
-        return reversedString;
-    }
-
-
-    public static List<int>? MergeSortedArrays(List<int> array1, List<int> array2)
-    {
-        ArrayValidation validation = IsInputValid(array1, array2);
-
-        if (validation.ShouldExit)
-            return validation.ReturnVal;
-
-        return MergeSortedInputArrays(array1, array2);
-    }
-
-    private static ArrayValidation IsInputValid(List<int> array1, List<int> array2)
-    {
-        if (array1 == null && array2 == null)
-            return new ArrayValidation(true, null);
-
-        if (array1 == null || array1.Count == 0)
-            return new ArrayValidation(true, array2);
-
-        if (array2 == null || array2.Count == 0)
-            return new ArrayValidation(true, array1);
-
-        return new ArrayValidation(false, null);
-    }
-
-    private class ArrayValidation
-    {
-        public ArrayValidation(bool shouldExit, List<int>? returnVal)
+        private static string ReverseStringInput(string input)
         {
-            ShouldExit = shouldExit;
-            ReturnVal = returnVal;
+            Stack<char> chars = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                chars.Push(c);
+            }
+
+            char[] reversedChars = chars.ToArray();
+
+            string reversedString = new string(reversedChars);
+
+            return reversedString;
         }
 
-        public bool ShouldExit { get; private set; }
-        public List<int>? ReturnVal { get; private set; }
-    }
 
-    private static List<int>? MergeSortedInputArrays(List<int> array1, List<int> array2)
-    {
-        int index1 = 0;
-        int index2 = 0;
-
-        List<int> mergedSortedArray = new List<int>();
-
-        bool finished = false;
-        while (!finished)
+        public static List<int>? MergeSortedArrays(List<int> array1, List<int> array2)
         {
-            bool index1InArray = index1 < array1.Count;
-            bool index2InArray = index2 < array2.Count;
+            ArrayValidation validation = IsInputValid(array1, array2);
 
-            if (!index1InArray && !index2InArray)
+            if (validation.ShouldExit)
+                return validation.ReturnVal;
+
+            return MergeSortedInputArrays(array1, array2);
+        }
+
+        private static ArrayValidation IsInputValid(List<int> array1, List<int> array2)
+        {
+            if (array1 == null && array2 == null)
+                return new ArrayValidation(true, null);
+
+            if (array1 == null || array1.Count == 0)
+                return new ArrayValidation(true, array2);
+
+            if (array2 == null || array2.Count == 0)
+                return new ArrayValidation(true, array1);
+
+            return new ArrayValidation(false, null);
+        }
+
+        private class ArrayValidation
+        {
+            public ArrayValidation(bool shouldExit, List<int>? returnVal)
             {
-                finished = true;
+                ShouldExit = shouldExit;
+                ReturnVal = returnVal;
             }
-            else if (!index1InArray)
+
+            public bool ShouldExit { get; private set; }
+            public List<int>? ReturnVal { get; private set; }
+        }
+
+        private static List<int>? MergeSortedInputArrays(List<int> array1, List<int> array2)
+        {
+            int index1 = 0;
+            int index2 = 0;
+
+            List<int> mergedSortedArray = new List<int>();
+
+            bool finished = false;
+            while (!finished)
             {
-                mergedSortedArray.Add(array2[index2]);
-                index2++;
-            }
-            else if (!index2InArray)
-            {
-                mergedSortedArray.Add(array1[index1]);
-                index1++;
-            }
-            else
-            {
-                if (array1[index1] < array2[index2])
+                bool index1InArray = index1 < array1.Count;
+                bool index2InArray = index2 < array2.Count;
+
+                if (!index1InArray && !index2InArray)
+                {
+                    finished = true;
+                }
+                else if (!index1InArray)
+                {
+                    mergedSortedArray.Add(array2[index2]);
+                    index2++;
+                }
+                else if (!index2InArray)
                 {
                     mergedSortedArray.Add(array1[index1]);
                     index1++;
                 }
                 else
                 {
-                    mergedSortedArray.Add(array2[index2]);
-                    index2++;
+                    if (array1[index1] < array2[index2])
+                    {
+                        mergedSortedArray.Add(array1[index1]);
+                        index1++;
+                    }
+                    else
+                    {
+                        mergedSortedArray.Add(array2[index2]);
+                        index2++;
+                    }
                 }
             }
-        }
 
-        return mergedSortedArray;
+            return mergedSortedArray;
+        }
     }
 }
---
+++ Sec 07 - Hashing/Questions.cs
@@ -0,0 +1,98 @@
+﻿namespace Sec07
+{
+    public class Questions
+    {
+        public static void Main(string[] args)
+        {
+            HashTable<string> hashTable = new HashTable<string>(50);
+
+            hashTable.Set("Hello", "World");
+
+            Console.WriteLine(hashTable.Get("Hello"));
+            Console.ReadKey();
+        }
+    }
+
+    public class HashTable<T>
+    {
+        public int Size { get; private set; }
+        private List<KeyValue>[] data;
+
+        public HashTable(int size)
+        {
+            Size = size;
+            data = new List<KeyValue>[size];
+        }
+
+        private int hash(string key)
+        {
+            int hash = 0;
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                hash = (hash + (int)key[i] * i) % Size;
+            }
+
+            return hash;
+        }
+
+        public bool Set(string key, T value)
+        {
+            int index = hash(key);
+
+            KeyValue elem = new KeyValue(key, value);
+
+            AddKeyValueToDataAtIndex(index, elem);
+
+            return true;
+        }
+
+        private void AddKeyValueToDataAtIndex(int index, KeyValue keyValue)
+        {
+            if (data[index] == null)
+            {
+                data[index] = new List<KeyValue> { keyValue };
+            }
+            else
+            {
+                List<KeyValue> keyValues = data[index];
+
+                for (int i = 0; i < keyValues.Count; i++)
+                {
+                    if (keyValues[i].Key == keyValue.Key)
+                    {
+                        keyValues[i] = keyValue;
+                        return;
+                    }
+                }
+
+                keyValues.Add(keyValue);
+            }
+        }
+
+        public T Get(string key)
+        {
+            List <KeyValue> keyValues = data[hash(key)];
+
+            foreach(KeyValue keyValue in keyValues)
+            {
+                if (keyValue.Key == key)
+                    return keyValue.Value;
+            }
+
+            throw new Exception("Key doesn't exist in the hashtable");
+        }
+
+        private class KeyValue
+        {
+            public string Key { get; private set; }
+            public T Value { get; private set; }
+
+            public KeyValue(string key, T value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+    }
+}
