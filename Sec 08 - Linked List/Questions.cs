8c7f3529cefde4e855f6615406a122de2e7b9f99
jamesacox53 <jamesacox53@gmail.com>
2022-10-08 20:27:54 +0100

    Completed Prepend

--- Sec 08 - Linked List/Questions.cs
+++ Sec 08 - Linked List/Questions.cs
@@ -6,11 +6,20 @@
         {
             LinkedList<int> linkedList = new LinkedList<int>();
 
+            Console.WriteLine("Append:");
+            
             linkedList.Append(5);
             linkedList.Append(16);
             linkedList.Append(20);
 
             Console.WriteLine(linkedList.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Prepend:");
+            linkedList.Prepend(44);
+
+            Console.WriteLine(linkedList.ToString());
+
             Console.ReadKey();
         }
     }
@@ -22,6 +31,26 @@
         public Node<T>? Tail { get; private set; }
 
         public int Length { get; private set; }
+
+        public LinkedList<T> Prepend(T value)
+        {
+            Node<T> node = new Node<T>(value, null);
+
+            if (Head == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.NextNode = Head;
+                Head = node;
+            }
+
+            Length++;
+
+            return this;
+        }
 
         public LinkedList<T> Append(T value)
         {
@@ -37,6 +66,8 @@
                 Tail.NextNode = node;
                 Tail = node;
             }
+
+            Length++;
 
             return this;
         }
