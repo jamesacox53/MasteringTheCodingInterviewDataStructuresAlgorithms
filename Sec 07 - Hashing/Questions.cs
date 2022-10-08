aa7636d99e547044deb0f6cfcedf3c7207cb699a
jamesacox53 <jamesacox53@gmail.com>
2022-10-08 15:57:30 +0100

    Completed Second Implementation of First Recurring Character 2

--- Sec 07 - Hashing/Questions.cs
+++ Sec 07 - Hashing/Questions.cs
@@ -28,7 +28,113 @@
                 Console.WriteLine(key);
             }
 
+            
+            List<char> chars1 = new List<char>() { '2', '5', '1', '2', '3', '5', '1', '2', '4' };
+            List<char> chars2 = new List<char>() { '2', '1', '1', '2', '3', '5', '1', '2', '4' };
+            List<char> chars3 = new List<char>() { '2', '3', '4', '5' };
+
+
+            Console.WriteLine();
+            Console.WriteLine("First Recurring Character1:");
+            
+            // should be 2
+            Console.WriteLine(GetFirstRecurringCharacter1(chars1));
+
+            // should be 1
+            Console.WriteLine(GetFirstRecurringCharacter1(chars2));
+
+            // should be null 
+            Console.WriteLine(GetFirstRecurringCharacter1(chars3));
+
+            Console.WriteLine();
+            Console.WriteLine("First Recurring Character2:");
+
+            // should be 2
+            Console.WriteLine(GetFirstRecurringCharacter2(chars1));
+
+            // should be 2
+            Console.WriteLine(GetFirstRecurringCharacter2(chars2));
+
+            // should be null
+            Console.WriteLine(GetFirstRecurringCharacter2(chars3));
+
             Console.ReadKey();
+        }
+
+        public static char? GetFirstRecurringCharacter1(List<char> characters)
+        {
+            HashSet<char> chars = new HashSet<char>();
+
+            foreach (char c in characters)
+            {
+                if (chars.Contains(c))
+                {
+                    return c;
+                }
+
+                chars.Add(c);
+            }
+
+            return null;
+        }
+
+        public static char? GetFirstRecurringCharacter2(List<char> characters)
+        {
+            LoadedDictionary loadedDictionaryObj = GetLoadedDictionary(characters);
+
+            if (loadedDictionaryObj.RepeatedFirstCharacter) 
+                return characters[0];
+
+            Dictionary<char, List<int>> loadedDictionary = loadedDictionaryObj.LoadedDictionaryElem;
+
+            for (int i = 1; i < characters.Count; i++)
+            {
+                List<int> indexes = loadedDictionary[characters[i]];
+
+                if (indexes.Count > 1) 
+                    return characters[i];
+            }
+
+            return null;
+        }
+
+        private static LoadedDictionary GetLoadedDictionary(List<char> characters)
+        {
+            Dictionary<char, List<int>> loadedDictionary = new Dictionary<char, List<int>>();
+
+            loadedDictionary.Add(characters[0], new List<int> {0});
+
+            for (int i = 1; i < characters.Count; i++)
+            {
+                char c = characters[i];
+
+                if (c == characters[0])
+                    return new LoadedDictionary(true, loadedDictionary);
+
+                if (loadedDictionary.ContainsKey(c))
+                {
+                    List<int> list = loadedDictionary[c];
+                    list.Add(i);
+                }
+                else
+                {
+                    loadedDictionary.Add(c, new List<int>() {i});
+                }
+            }
+
+            return new LoadedDictionary(false, loadedDictionary);
+        }
+
+        private class LoadedDictionary
+        {
+            public bool RepeatedFirstCharacter { get; private set; }
+            public Dictionary<char, List<int>> LoadedDictionaryElem { get; private set; }
+
+            public LoadedDictionary(bool repeatedFirstCharacter, Dictionary<char, List<int>> loadedDictionary)
+            {
+                RepeatedFirstCharacter = repeatedFirstCharacter;
+                LoadedDictionaryElem = loadedDictionary;
+            }
         }
     }
 
